b5772a6781c49b3db8bef21e48d71690aac1f73f
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-06-12 17:39:35 +0200

    delete graphql basic

--- app/graphql/types/article_type.rb
+++
@@ -1,6 +0,0 @@
-class Types::ArticleType < Types::BaseObject
-	graphql_name 'Article'
-	field :id, ID, null: false
-	field :title, String, null: false
-	field :description, String, null: false
-end
--- app/graphql/types/mutation_type.rb
+++
@@ -1,6 +0,0 @@
-module Types
-  class MutationType < Types::BaseObject
-    field :deleteArticle, mutation: Mutations::DeleteArticle
-    field :createArticle, mutation: Mutations::CreateArticle
-  end
-end
