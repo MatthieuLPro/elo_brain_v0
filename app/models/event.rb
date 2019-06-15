ea3499fa53738be68e14c994d0f48989bbed478c
MatthieuLPro <matthieu.lu.pro@gmail.com>
2019-06-15 19:01:39 +0200

    correction event match association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+	has_many :matches
 	validates :sgtournoi_id, :sgevent_id, :tournoi_name, :tournoi_date, :tournoi_place, :event_game, presence: true
 	validates :sgevent_id, uniqueness: true 
 end
