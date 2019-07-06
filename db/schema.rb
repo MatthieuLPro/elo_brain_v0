ad3a2e6c6bbd7bc7c09be432b16757dac48b99e8
geraldy <geraldy.leondas@gmail.com>
2019-07-06 17:49:38 +0200

    footer maj

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2019_06_15_155211) do
     t.integer "player_id"
     t.integer "match_id"
     t.string "saison"
-    t.string "platform"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["match_id"], name: "index_elos_on_match_id"
@@ -34,7 +33,6 @@ ActiveRecord::Schema.define(version: 2019_06_15_155211) do
     t.integer "nb_match"
     t.string "event_game"
     t.string "saison"
-    t.string "platform"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -62,8 +60,8 @@ ActiveRecord::Schema.define(version: 2019_06_15_155211) do
     t.integer "nb_match_saison"
     t.integer "nb_tournoi_saison"
     t.string "place"
-    t.string "ranking_previous_pc"
-    t.string "ranking_previous_ps4"
+    t.string "ranking_record"
+    t.string "ranking_previous"
     t.string "saison_current"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
